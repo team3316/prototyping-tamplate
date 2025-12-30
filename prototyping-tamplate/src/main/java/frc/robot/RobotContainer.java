47dfc8fb0c909cf8b1272b8a2084213fa1d666f4
UriAvidan <aavidan.uri@gmail.com>
2025-12-30 17:12:19 +0200

    drive robot container

--- prototyping-tamplate/src/main/java/frc/robot/RobotContainer.java
+++ prototyping-tamplate/src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,12 @@
 
 package frc.robot;
 
+import frc.robot.constants.DriveConstants;
 import frc.robot.constants.JoysticksConstants;
+import frc.robot.subsystems.drive.Drivetrain;
+import frc.robot.subsystems.drive.ModuleIOSpark;
+import frc.robot.subsystems.drive.GyroIOPigeon;
+import frc.robot.subsystems.drive.Module.ModuleLocation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
@@ -16,12 +21,29 @@ import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
  */
 public class RobotContainer {
 
-
+  private boolean isFieldRelative;
+  private final Drivetrain m_drivetrain;
   private final CommandPS5Controller m_controller = new CommandPS5Controller(JoysticksConstants.DRIVER_CONTROLLER_PORT);
 
   public RobotContainer() {
-  
+          isFieldRelative = true;
 
+      m_drivetrain = new Drivetrain(
+        new GyroIOPigeon(DriveConstants.GYRO_PORT),
+        new ModuleIOSpark(ModuleLocation.FL),
+        new ModuleIOSpark(ModuleLocation.FR),
+        new ModuleIOSpark(ModuleLocation.BL),
+        new ModuleIOSpark(ModuleLocation.BR)
+      );
+
+  
+    m_drivetrain.setDefaultCommand(Commands.run(() -> m_drivetrain.runVelocity(
+      m_controller.getLeftY() * DriveConstants.MAX_SPEED_METERS_PER_SECOND,
+      m_controller.getLeftX() * DriveConstants.MAX_SPEED_METERS_PER_SECOND,
+      m_controller.getRightX() * DriveConstants.MAX_ANGULAR_VELOCITY_RAD_PER_SEC
+      ,isFieldRelative
+    ),
+    m_drivetrain));
     configureBindings();
   }
 
