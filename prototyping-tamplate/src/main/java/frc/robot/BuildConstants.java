043e920bac7c06f80e88cfb42aa28b681287163e
UriAvidan <aavidan.uri@gmail.com>
2025-12-30 15:36:15 +0200

    AK init

---
+++ prototyping-tamplate/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "offseason-2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 483;
+  public static final String GIT_SHA = "e5392babf3e89c2eb0bed327afc86df9e1d9639b";
+  public static final String GIT_DATE = "2025-10-16 03:51:01 EDT";
+  public static final String GIT_BRANCH = "ios";
+  public static final String BUILD_DATE = "2025-10-18 07:34:10 EDT";
+  public static final long BUILD_UNIX_TIME = 1760787250617L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
