2bc339332b9facddba7aa50a39d1ce612b82bb9d
UriAvidan <aavidan.uri@gmail.com>
2025-12-30 16:35:47 +0200

    Drive removed

--- prototyping-tamplate/src/main/java/frc/robot/RobotContainer.java
+++ prototyping-tamplate/src/main/java/frc/robot/RobotContainer.java
@@ -4,22 +4,10 @@
 
 package frc.robot;
 
-
-import frc.robot.constants.Constants;
-import frc.robot.constants.DriveConstants;
 import frc.robot.constants.JoysticksConstants;
-import frc.robot.subsystems.drive.Drivetrain;
-import frc.robot.subsystems.drive.ModuleIOSpark;
-import frc.robot.subsystems.drive.GyroIO;
-import frc.robot.subsystems.drive.GyroIOPigeon;
-import frc.robot.subsystems.drive.Module.ModuleLocation;
-import frc.robot.subsystems.drive.ModuleIO;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -28,49 +16,11 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
 
-  private final Drivetrain m_drivetrain;
-  private boolean isFieldRelative;
+
   private final CommandPS5Controller m_controller = new CommandPS5Controller(JoysticksConstants.DRIVER_CONTROLLER_PORT);
 
   public RobotContainer() {
-    switch(Constants.currentMode){
-      case REAL:
-        isFieldRelative = true;
-        m_drivetrain = new Drivetrain(
-          new GyroIOPigeon(DriveConstants.GYRO_PORT),
-          new ModuleIOSpark(ModuleLocation.FL),
-          new ModuleIOSpark(ModuleLocation.FR),
-          new ModuleIOSpark(ModuleLocation.BL),
-          new ModuleIOSpark(ModuleLocation.BR)
-        );
-        break;
-      case SIM:
-            m_drivetrain = new Drivetrain(
-        new GyroIO() {},
-        new ModuleIO() {},
-        new ModuleIO() {},
-        new ModuleIO() {},
-        new ModuleIO() {}
-      );
-      break;
-    default:
-      m_drivetrain = new Drivetrain(
-        new GyroIO() {},
-        new ModuleIO() {},
-        new ModuleIO() {},
-        new ModuleIO() {},
-        new ModuleIO() {}
-      );
-    }
-
-
-    m_drivetrain.setDefaultCommand(Commands.run(() -> m_drivetrain.runVelocity(
-      m_controller.getLeftY() * DriveConstants.MAX_SPEED_METERS_PER_SECOND,
-      m_controller.getLeftX() * DriveConstants.MAX_SPEED_METERS_PER_SECOND,
-      m_controller.getRightX() * DriveConstants.MAX_ANGULAR_VELOCITY_RAD_PER_SEC
-      ,isFieldRelative
-    ),
-    m_drivetrain));
+  
 
     configureBindings();
   }
